1070e0fbaedfeb3917886f02fa1d5032246d7cab
umarazzam10 <145319150+umarazzam10@users.noreply.github.com>
2023-11-12 23:31:30 +0700

    Update App.java

--- 09-interface-umarazzam10/src/main/java/id/ac/unand/fti/si/pbo/App.java
+++ 09-interface-umarazzam10/src/main/java/id/ac/unand/fti/si/pbo/App.java
@@ -14,21 +14,25 @@ public class App {
         MemberGold member7 = new MemberGold();
         MemberPlatinum member8 = new MemberPlatinum();
 
-        
+        //menghitung total bayar member
         int totalBayarReguler = member1.hitungTotalBayar(1000000);
         int totalBayarSilver = member2.hitungTotalBayar(3500000);
         int totalBayarGold = member3.hitungTotalBayar(7000000);
         int totalBayarPlatinum = member4.hitungTotalBayar(1500000);
 
+        //menghitung ongkir member
         int ongkirPlatinum = member8.hitungOngkir(20.5);
         int ongkirGold = member7.hitungOngkir(15.3);
         int ongkirSilver = member6.hitungOngkir(10.0);
 
+        //menghitung cicilan member
         int cicilanPlatinum = member8.hitungCicilanPerBulan(totalBayarPlatinum, 5);
         int cicilanGold = member8.hitungCicilanPerBulan(totalBayarGold, 7);
 
+        //cek saldo member
         int Saldo = member8.cekSaldo();
-        
+
+        //menampilkan data struk member
         System.out.println("________________________________________");
         System.out.println("Total bayar member reguler: " + totalBayarReguler);
         System.out.println("________________________________________");
